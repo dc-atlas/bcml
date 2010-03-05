c3aac59708a628a984aee5c23717cb9835ed0981
Luca Beltrame <luca.beltrame@marionegri.it>
2010-03-05 16:18:56 +0000

    Fix build on Linux

--- build.sh
+++ build.sh
@@ -1,37 +1,26 @@
+#!/bin/sh
+
 mvn clean install
-
 cd checksbgn
-
 mvn assembly:assembly
 
 cd ..
-
 cd graphmlconvert
-
 mvn assembly:assembly
-
 cd ..
 
 
 cd exporter
-
 mvn assembly:assembly
-
 cd ..
 
 
 cd filter
-
 mvn assembly:assembly
-
 cd ..
 
 
 cp checksbgn/target/checksbgn-1.0-SNAPSHOT-jar-with-dependencies.jar .
-
 cp graphmlconvert/target/graphmlconvert-1.0-SNAPSHOT-jar-with-dependencies.jar .
-
 cp exporter/target/exporter-1.0-SNAPSHOT-jar-with-dependencies.jar .
-
 cp filter/target/filter-1.0-SNAPSHOT-jar-with-dependencies.jar .
-
